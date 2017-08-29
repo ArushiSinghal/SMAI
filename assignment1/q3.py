fbdea7ca57493505e43b3db88fdaa7e86dbc6361
Arushi Singhal <arushisinghal@gmail.com>
2017-08-29 21:15:11 +0530

    q3

--- assignment1/q3.py
+++ assignment1/q3.py
@@ -13,69 +13,32 @@ def load_csv(filename):
     result = numpy.delete(result, 0, 0)
     col = result[:,6]
     result = numpy.delete(result, 6, 1)
-    return result, col
+    result = numpy.hstack((result,col))
+    #print result
+    return result
 
-answer = []
-answer1 = []
-answer2 = []
-answer3 = []
-answer4 = []
-answer5 = []
-answer6 = []
-answer7 = []
-answer8 = []
-dataset_train = 'datasets/q3/train.csv'
-dataset_test = 'datasets/q3/test.csv'
-result , label = load_csv(dataset_train)
-result1, label1 = load_csv(dataset_test)
-print (result)
-num_rows, num_cols = result.shape
+def load_csv1(filename):
+    csv_reader = reader(open(filename, 'rt'))
+    x = list(csv_reader)
+    result = numpy.array(x)
+    result = numpy.delete(result, 0, 0)
+    #print result
+    return result
 
-for i in range(num_rows):
-	if result[i][0] not in answer:
-		answer.append(result[i][0])
-print (answer)
-for i in range(num_rows):
-        if result[i][1] not in answer1:
-                answer1.append(result[i][1])
-print (answer1)
-for i in range(num_rows):
-        if result[i][2] not in answer2:
-                answer2.append(result[i][2])
-print (answer2)
-for i in range(num_rows):
-        if result[i][3] not in answer3:
-                answer3.append(result[i][3])
-print (answer3)
-for i in range(num_rows):
-        if result[i][4] not in answer4:
-                answer4.append(result[i][4])
-print (answer4)
-for i in range(num_rows):
-        if result[i][5] not in answer5:
-                answer5.append(result[i][5])
-print (answer5)
-for i in range(num_rows):
-        if result[i][6] not in answer6:
-                answer6.append(result[i][6])
-print (answer6)
+def unique(result):
+    answer = []
+    num_rows,num_col = result.shape
+    for i in range(num_cols-1):
+        for k in range(num_row):
+            if result[k][i] not in answer[i]:
+                answer[i].append(result[k][i])
+    return answer
 
-for i in range(num_rows):
-    if result[i][7] not in answer7:
-        answer7.append(result[i][7])
-print (answer7)
-
-for i in range(num_rows):
-        if result[i][8] not in answer8:
-                answer8.append(result[i][8])
-print (answer8)
-
-######################## Calculate entropy for all cases ############
-def wholeH(label):
+def wholeH(result):
 	count_leave = 0
 	count_stay = 0
 	for i in range(num_rows):
-		if label[i] == '0':
+		if result[9][i] == '0':
 			count_stay += 1;
 		else:
 			count_leave +=1
@@ -83,217 +46,200 @@ def wholeH(label):
 	neg = (count_stay*1.0)/(count_stay+count_leave)
 	p = -1*(pos*math.log(pos,2) + neg*math.log(neg,2))
 	return p
-################################# Calculate information gain for all satisfaction level #############
-def countimpormationgain(col,H,val):
-	num_rows = len(col)
-	count1 = 0
-	count2 = 0
-	count3 = 0
-	count4 = 0
-	pa = 0 
-	pb = 0
-	for i in range(num_rows):
-    		if float(col1[i]) <= val:
-			count1 += 1
-    		if float(col1[i]) <= val and label[i] == '1':
-			count2 += 1
-    		if float(col1[i]) > val:
-			count3 += 1
-    		if float(col1[i]) > val and label[i] == '1':
-			count4 += 1
-	if (count3 ==0 or count1 ==0):
-		return 0
-	p3 = (count2*1.0)/count1
-	p4 = (count1-count2)/(count1*1.0)
-	p5 = (count4*1.0)/count3
-        p6 = (count3-count4)/(count3*1.0)
-	p7 = count1 + count3
-	if (p3 == 0 or p3 ==1):
-		pa = 0
-	else:
-		pa = -1*(p3*math.log(p3,2) + p4*math.log(p4,2))
-	if (p5 == 0 or p5 ==1):
-		pb = 0
-	else:
-		pb = -1*(p5*math.log(p5,2) + p6*math.log(p6,2))
-    	p1 = ((count1*1.0)/p7)*pa + ((count3*1.0)/p7)*pb
-	inf_gain= H - p1
-	return inf_gain
 
-H = wholeH(label)
-print ("############SATISFACTION LEVEL########################################")
-col1 = result[:,0]
-c = 0
-inf_gain = 0
-pa = 0
-pb =0
-for i in range(len(answer)):
-	print len(answer)
-	inf_gain = countimpormationgain(col1, H, float(answer[i]))
-	if inf_gain  > c:
-		c = inf_gain
-		d = answer[i]
-		#pa_new = pa
-		#pb_new = pb
-#print(pa_new)
-#print(pb_new)
-print(d)
-print (c)
-print ("################WORK ACCIDENTS####################################")
-col1 = result[:,5]
-inf_gain = countimpormationgain(col1, H, 0)
-print(inf_gain)
-print ("###########PROMOTION LAST FIVE YEAR#########################################")
-col1 = result[:,6]
-inf_gain = countimpormationgain(col1, H, 0)
-print(inf_gain)
-print ("###########NUMBER OF PROJECTS#########################################")
-col1 = result[:,2]
-c = 0
-for i in range(len(answer2)):
-	inf_gain = countimpormationgain(col1, H, float(answer2[i]))
-	if inf_gain > c:
-		c = inf_gain
-		d = answer2[i]
-print(d)
-print(c)
-print ("###########LAST EVALUATION#########################################")
-col1 = result[:,1]
-c = 0
-for i in range(len(answer1)):
-	inf_gain = countimpormationgain(col1, H, float(answer1[i]))
-	if inf_gain > c:
-		c = inf_gain
-                d = answer1[i]
-print(d)
-print(c)
+def to_terminal(node):
+    num_rows, num_cols = node.shape
+    count = 0
+    count1 = 0
+    for i in range(num_rows):
+        if int(node[i][9]) == 1:
+            count +=1
+        if int(node[i][9]) == 0:
+            count1 +=1
+    if count >= count1:
+        return 1
+    else:
+        return 0
 
-print ("###########AVERAGE MONTHLY HOURS#########################################")
-col1 = result[:,3]
-c = 0
-for i in range(len(answer3)):
-        inf_gain = countimpormationgain(col1, H, float(answer3[i]))
-        if inf_gain > c:
-                c = inf_gain
-                d = answer3[i]
-print(d)
-print(c)
-print("#########SALARY###########################################")
-col1 = result[:,8]
-count1 = 0
-count2 = 0
-count3 = 0
-count4 = 0
-count5 = 0
-count6 = 0
-for i in range(num_rows):
-    if col1[i] == "low":
-        count1 += 1
-    if col1[i] == "low" and label[i] == '0':
-        count2 +=1
-    if col1[i] == "medium":
-        count3 +=1
-    if col1[i] == "medium" and label[i] == '0':
-        count4 +=1
-    if col1[i] == "high":
-        count5 +=1
-    if col1[i] == "high" and label[i] == '0':
-        count6 +=1
-p3 = (count2*1.0)/count1
-p4 = (count1-count2)/(count1*1.0)
-p5 = (count4*1.0)/count3
-p6 = (count3-count4)/(count3*1.0)
-p7 = count1 + count3 + count5
-p8 = (count6*1.0)/count5
-p9 = (count5 - count6)/(count5*1.0)
-p1 = -1*((count1*1.0)/p7)*(p3*math.log(p3,2) + p4*math.log(p4,2)) + -1*((count3*1.0)/p7)*(p5*math.log(p5,2) + p6*math.log(p6,2)) + -1*((count5*1.0)/p7)*(p8*math.log(p8,2) + p9*math.log(p9,2))
-inf_gain = H - p1
-print (inf_gain)
-print ("###########SALES#########################################")
-'sales', 'accounting', 'technical', 'management', 'IT', 'product_mng', 'marketing', 'RandD', 'support', 'hr'
-col1 = result[:,7]
-count1 = 0
-count2 = 0
-count3 = 0
-count4 = 0
-count5 = 0
-count6 = 0
-count7 = 0
-count8 = 0
-count9 = 0
-count10 = 0
-count11 = 0
-count12 = 0
-count13 = 0
-count14 = 0
-count15 = 0
-count16 = 0
-count17 = 0
-count18 = 0
-count19 = 0
-count20 = 0
-for i in range(num_rows):
-    if (col1[i]) == answer7[0]:
-        count1 +=1
-    if (col1[i]) == answer7[0] and label[i] == '0':
-        count2 +=1
-    if (col1[i]) == answer7[1]:
-        count3 +=1
-    if (col1[i]) == answer7[1] and label[i] == '0':
-        count4 +=1
-    if (col1[i]) == answer7[2]:
-        count5 +=1
-    if (col1[i]) == answer7[2] and label[i] == '0':
-        count6 +=1
-    if (col1[i]) == answer7[3]:
-        count7 +=1
-    if (col1[i]) == answer7[3] and label[i] == '0':
-        count8 +=1
-    if (col1[i]) == answer7[4]:
-        count9 +=1
-    if (col1[i]) == answer7[4] and label[i] == '0':
-        count10 +=1
-    if (col1[i]) == answer7[5]:
-        count11 +=1
-    if (col1[i]) == answer7[5] and label[i] == '0':
-        count12 +=1
-    if (col1[i]) == answer7[6]:
-        count13 +=1
-    if (col1[i]) == answer7[6] and label[i] == '0':
-        count14 +=1
-    if (col1[i]) == answer7[7]:
-        count15 +=1
-    if (col1[i]) == answer7[7] and label[i] == '0':
-        count16 +=1
-    if (col1[i]) == answer7[8]:
-        count17 +=1
-    if (col1[i]) == answer7[8] and label[i] == '0':
-        count18 +=1
-    if (col1[i]) == answer7[9]:
-        count19 +=1
-    if (col1[i]) == answer7[9] and label[i] == '0':
-        count20 +=1
-p3 = (count2*1.0)/count1
-p4 = (count1-count2)/(count1*1.0)
-p5 = (count4*1.0)/count3
-p6 = (count3-count4)/(count3*1.0)
-p8 = (count6*1.0)/count5
-p9 = (count5 - count6)/(count5*1.0)
-p10 = (count8*1.0)/count7
-p11 = (count7 - count8*1.0)/count7
-p12 = (count10*1.0)/count9
-p13 = (count9 - count10*1.0)/count9
-p14 = (count12*1.0)/count11
-p15 = (count11 - count12*1.0)/count11
-p16 = (count14*1.0)/count13
-p17 = (count13 - count14*1.0)/count13
-p18 = (count16*1.0)/count15
-p19 = (count15 - count16*1.0)/count15
-p20 = (count18*1.0)/count17
-p21 = (count17 - count18*1.0)/count17
-p22 = (count20*1.0)/count19
-p23 = (count19 - count20*1.0)/count19
-p7 = count1 + count3 + count5 + count7 + count9 + count11 + count13 + count15 + count17 + count19
-p1 = -1*((count1*1.0)/p7)*(p3*math.log(p3,2) + p4*math.log(p4,2)) + -1*((count3*1.0)/p7)*(p5*math.log(p5,2) + p6*math.log(p6,2)) + -1*((count5*1.0)/p7)*(p8*math.log(p8,2) + p9*math.log(p9,2)) + -1*((count7*1.0)/p7)*(p10*math.log(p10,2) + p11*math.log(p11,2)) + -1*((count9*1.0)/p7)*(p12*math.log(p12,2) + p13*math.log(p13,2)) + -1*((count11*1.0)/p7)*(p14*math.log(p14,2) + p15*math.log(p15,2)) + -1*((count13*1.0)/p7)*(p16*math.log(p16,2) + p17*math.log(p17,2)) + -1*((count15*1.0)/p7)*(p18*math.log(p18,2) + p19*math.log(p19,2)) + -1*((count17*1.0)/p7)*(p20*math.log(p20,2) + p21*math.log(p21,2)) + -1*((count19*1.0)/p7)*(p22*math.log(p22,2) + p23*math.log(p23,2)) 
-inf_gain = H - p1
-print (inf_gain)
+def get_tree(node):
+    left, right = node['groups']
+    del(node['groups'])
+    if (not left):
+        node['left'] = to_terminal(right)
+    if (not right):
+        node['right'] = to_terminal(left)
+    if len(left) > 0 and len(left) <= 100:
+        node['left'] = to_terminal(left)
+    elif len(left) > 100:
+        node['left'] = gainfunction(left)
+        get_tree(node['left'])
+    if len(right) > 0 and len(right) <= 100:
+        node['right'] = to_terminal(right)
+    elif len(right) > 100:
+        node['right'] = gainfunction(right)
+        get_tree(node['right'])
+
+def test(node, row):
+    num_rows,num_col = result.shape
+    if node['index'] < 7:
+        if row[node['index']] <= node['value']:
+            if isinstance(node['left'], dict):
+                return test(node['left'], row)
+            else:
+                print (node['left'])
+                return node['left']
+        else:
+            if isinstance(node['right'], dict):
+                return test(node['right'], row)
+            else:
+                print (node['right'])
+                return node['right']
+   else:
+       if row[node['index']] != node['value']:
+           if isinstance(node['left'], dict):
+               return test(node['left'], row)
+           else:
+               return node['left']
+       else:
+           if isinstance(node['right'], dict):
+               return test(node['right'], row)
+           else:
+               return node['right']
+
+def split (result, index, val):
+    left = []
+    right = []
+    num_rows,num_col = result.shape
+    if index < 7:
+        for j in range(num_rows):
+            if result[j][index] <= val:
+                left.append(result[j])
+            else:
+                rigth.append(result[j])
+    else:
+        for j in range(num_rows):
+            if result[j][index] != val:
+                left.append(result[j])
+            else:
+                rigth.append(result[j])
+    left = numpy(left)
+    right = numpy(right)
+    groups = left, right
+    b_index, b_value, b_groups = index, val, groups
+    return {'index':b_index, 'value':b_value, 'groups':b_groups}
+
+def gainfunction(result):
+    H = wholeH(result)
+    answer = unique(result)
+    num_rows,num_col = result.shape
+    energy_gain = -1
+    index = 0
+    val = 0
+    for k in range(num_col-2):
+    	count1 = 0
+    	count2 = 0
+    	count3 = 0
+    	count4 = 0
+    	pa = 0
+    	pb = 0
+        for j in range(len(answer[k])):
+            for i in range(num_rows):
+                if float(result[i][k]) <= float(answer[k][j]):
+                    count1 += 1
+                if float(result[i][k]) <= float(answer[k][j]) and result[i][9] == '1':
+                    count2 += 1
+                if float(result[i][k]) > float(answer[k][j]):
+                    count3 += 1
+                if float(result[i][k]) > float(answer[k][j]) and result[i][9] == '1':
+                    count4 += 1
+            p3 = (count2*1.0)/count1
+            p4 = (count1-count2)/(count1*1.0)
+            p5 = (count4*1.0)/count3
+            p6 = (count3-count4)/(count3*1.0)
+            p7 = count1 + count3
+            if (p3 == 0 or p3 ==1):
+            	pa = 0
+            else:
+            	pa = -1*(p3*math.log(p3,2) + p4*math.log(p4,2))
+            if (p5 == 0 or p5 ==1):
+            	pb = 0
+            else:
+            	pb = -1*(p5*math.log(p5,2) + p6*math.log(p6,2))
+            p1 = ((count1*1.0)/p7)*pa + ((count3*1.0)/p7)*pb
+            inf_gain= H - p1
+            if inf_gain > energy_gain:
+                energy_gain = inf_gain
+                index = i
+                val = float(answer[i][j])
+    i = 7
+    while  i < num_col:
+        count1 = 0
+    	count2 = 0
+    	count3 = 0
+    	count4 = 0
+    	pa = 0
+    	pb = 0
+        for j in range(len(answer[i])):
+            for k in range(num_rows):
+                if result[k][i] != answer[i][j]:
+                    count1 += 1
+                if result[k][i] != answer[i][j] and result[i][9] == '1':
+                    count2 += 1
+                if result[k][i] == answer[i][j]:
+            		count3 += 1
+                if result[k][i] == answer[i][j] and result[i][9] == '1':
+            		count4 += 1
+            if (count1 == 0 and count3 != 0):
+                pa = 0
+                p5 = (count4*1.0)/count3
+                p6 = (count3-count4)/(count3*1.0)
+                p7 = count1 + count3
+                if (p5 == 0 or p5 ==1):
+                	pb = 0
+                else:
+                	pb = -1*(p5*math.log(p5,2) + p6*math.log(p6,2))
+
+            elif (count1 == 0 and count3 == 0):
+                pa = 0
+                pb = 0
+            elif (count1 != 0 and count3 == 0):
+                pb = 0
+                p3 = (count2*1.0)/count1
+                p4 = (count1-count2)/(count1*1.0)
+                p7 = count1 + count3
+                if (p3 == 0 or p3 ==1):
+                	pa = 0
+                else:
+                	pa = -1*(p3*math.log(p3,2) + p4*math.log(p4,2))
+            else:
+                p3 = (count2*1.0)/count1
+                p4 = (count1-count2)/(count1*1.0)
+                p5 = (count4*1.0)/count3
+                p6 = (count3-count4)/(count3*1.0)
+                p7 = count1 + count3
+                if (p3 == 0 or p3 ==1):
+            	       pa = 0
+                else:
+            	       pa = -1*(p3*math.log(p3,2) + p4*math.log(p4,2))
+                if (p5 == 0 or p5 ==1):
+            	       pb = 0
+                else:
+            	       pb = -1*(p5*math.log(p5,2) + p6*math.log(p6,2))
+            p1 = ((count1*1.0)/p7)*pa + ((count3*1.0)/p7)*pb
+            inf_gain= H - p1
+            if inf_gain > energy_gain:
+                energy_gain = inf_gain
+                index = i
+                val = answer[i][j]
+        i += 1
+    return split(result,index,val)
+
+dataset_train = sys.argv[1]
+dataset_test = sys.argv[2]
+result = load_csv(dataset_train)
+result1 = load_csv1(dataset_test)
+root = gainfunction(result)
+tree = get_tree(root)
+num_row, num_col = result1.shape
+
+test(root)
